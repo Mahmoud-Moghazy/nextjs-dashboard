5831b99a29d347b14d1570feea1277f866656080
Mahmoud-Moghazy <ma.moghazyggmail.com>
2024-06-17 02:29:02 +0300

    final commite

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import Image from 'next/image';
 
 import styles from '@/app/ui/home.module.css';
 
+
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
